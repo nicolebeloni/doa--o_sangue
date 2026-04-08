4531641b47614171e81b64b067fb2d493946c317
nicole <nicole_b_silva@estudante.sesisenai.org>
2026-04-08 12:18:44 -0300

    adicionei coisas no js

--- script.js
+++ script.js
@@ -9,4 +9,24 @@ function formulario () {
     let estado= document.getElementById("estado").value;
 }
 
+ if (nome.indexOf(" ") == -1) {
+        alert("Informe nome e sobrenome!");
+        return;
+    }
 
+    if (idade < 16) {
+        alert("Idade mínima é 16 anos!");
+        return;
+    }
+
+    if (peso < 50) {
+        alert("Peso mínimo é 50kg!");
+        return;
+    }
+
+    if (isNaN(telefone)) {
+        alert("Telefone deve ter apenas números!");
+        return;
+    }
+
+    alert("Cadastro realizado com sucesso!");
