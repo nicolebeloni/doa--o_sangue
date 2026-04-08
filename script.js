26529c90a34de69fd7f65bd4e3ee43dfeac61673
nicole <nicole_b_silva@estudante.sesisenai.org>
2026-04-08 11:54:42 -0300

    add- fiz a function

---
+++ script.js
@@ -0,0 +1,12 @@
+function formulario () {
+    let nome= document.getElementById("nome").value;
+    let email= document.getElementById("email").value;
+    let idade= document.getElementById("idade").value;
+    let peso= document.getElementById("peso").value;
+    let sanguineo= document.getElementById("sanguineo").value;
+    let telefone= document.getElementById("telefone").value;
+    let cidade= document.getElementById("cidade").value;
+    let estado= document.getElementById("estado").value;
+}
+
+
